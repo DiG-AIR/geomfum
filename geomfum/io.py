a79ee764502beb4a21e3dae6988032395ceaa360
gviga <giulio.vigan@gmail.com>
2025-02-18 09:54:23 +0100

    Add input torch

--- geomfum/io.py
+++ geomfum/io.py
@@ -1,4 +1,5 @@
 import meshio
+import torch
 
 
 def load_mesh(filename):
@@ -16,3 +17,19 @@ def load_mesh(filename):
     """
     mesh = meshio.read(filename)
     return mesh.points, mesh.cells[0].data
+
+def load_mesh_torch(filename):
+    """Load a mesh from a file.
+
+    Parameters
+    ----------
+    filename : str
+        File name.
+
+    Returns
+    -------
+    vertices : array-like, shape=[n_vertices, 3]
+    faces : array_like, shape=[n_faces, 3]
+    """
+    mesh = meshio.read(filename)
+    return torch.tensor(mesh.points),torch.tensor(mesh.cells[0].data)
