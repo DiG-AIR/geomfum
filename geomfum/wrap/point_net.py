ecdd009285bdd45367ac2b0e0c4060b39e4b8765
gviga <giulio.vigan@gmail.com>
2025-03-21 10:02:11 +0100

    Update Learned Descriptors Wrapper
    I've removed the feature extractor folder so that everithing needs to be implemented in the wrap files. This make the code more messy so we can discuss.

--- geomfum/wrap/point_net.py
+++ geomfum/wrap/point_net.py
@@ -4,7 +4,6 @@ This is the wrapper of the PointNet model.
 #TODO: For the moment we assume to have the implementation of PointNet somewhere in the code
 """
 
-from geomfum.feature_extractor.point_net.pointnet import PointNet  
 from geomfum.descriptor._base import LearnedDescriptor
 from geomfum.shape.mesh import TriangleMesh
 import torch
@@ -13,7 +12,7 @@ import torch
 class PointNetDescriptor(torch.nn.Module,LearnedDescriptor):
     """Descriptor representing the output of PointNet."""
 
-    def __init__(self, k=128,device=torch.device('cuda'), feature_transform=False):
+    def __init__(self, k=128,device=torch.device('cpu'), feature_transform=False):
         super(PointNetDescriptor, self).__init__()
         self.model = PointNet(k=k, feature_transform=feature_transform).to(device)
         self.n_features = k
@@ -65,3 +64,79 @@ class PointNetDescriptor(torch.nn.Module,LearnedDescriptor):
     def load(self, premodel):
         #load model parameters from the provided path
         self.model.load_state_dict(premodel)
+
+
+
+"""
+Implementation of PointNet feature extractors from 
+https://github.com/riccardomarin/Diff-FMaps by Riccardo Marin
+"""
+import torch
+import torch.nn as nn
+import torch.nn.parallel
+import torch.utils.data
+import torch.nn.functional as F
+
+
+class PointNetfeat(nn.Module):
+
+    def __init__(self, global_feat = True, feature_transform = False):
+
+        super(PointNetfeat, self).__init__()
+
+        self.conv1 = torch.nn.Conv1d(3, 64, 1)
+        self.conv2 = torch.nn.Conv1d(64, 64, 1)
+        self.conv4 = torch.nn.Conv1d(64, 128, 1)
+        self.conv41 = torch.nn.Conv1d(128, 128, 1)
+        self.conv5 = torch.nn.Conv1d(128, 1024, 1)
+
+        self.dense1 = torch.nn.Linear(1024,256)
+        self.dense2 = torch.nn.Linear(256,256)
+
+        self.global_feat = global_feat
+        self.feature_transform = feature_transform
+
+    def forward(self, x):
+
+        n_pts = x.size()[2]
+        x = F.relu((self.conv1(x)))
+        x = F.relu((self.conv2(x)))
+        x = F.relu((self.conv4(x)))
+        x = F.relu((self.conv41(x)))
+        x = F.relu((self.conv5(x)))
+        pointfeat = x
+        x = torch.max(x, 2, keepdim=True)[0]
+        x = x.view(-1, 1024)
+        x = F.relu((self.dense1(x)))
+        x = F.relu((self.dense2(x)))
+
+        trans_feat = None
+        trans = None
+        x = x.view(-1, 256, 1).repeat(1, 1, n_pts)
+        return torch.cat([x, pointfeat], 1), trans, trans_feat
+
+class PointNet(nn.Module):
+    
+    def __init__(self, k = 128, feature_transform=False):
+        super(PointNet, self).__init__()
+        self.k = k
+        self.feature_transform=feature_transform
+        self.feat = PointNetfeat(global_feat=False, feature_transform=feature_transform)
+        self.conv1 = torch.nn.Conv1d(1280, 512, 1)
+        self.conv2 = torch.nn.Conv1d(512, 256, 1)
+        self.conv2c = torch.nn.Conv1d(256, 256, 1)
+        self.conv3 = torch.nn.Conv1d(256, self.k, 1)
+        self.m   = nn.Dropout(p=0.3)
+    def forward(self, x):
+        batchsize = x.size()[0]
+        n_pts = x.size()[2]
+        x, trans, trans_feat = self.feat(x)
+        x = F.relu((self.conv1(x)))
+        x = F.relu((self.conv2(x)))
+        x = F.relu((self.conv2c(x)))
+        x = self.m(x)
+        x = self.conv3(x)
+        x = x.transpose(2,1).contiguous()
+        x = x.view(batchsize, n_pts, self.k)
+        return x
+
